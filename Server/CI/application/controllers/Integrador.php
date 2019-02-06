fd85f692d4c3fd9ca6a77cd3a4c2dbe9482e2349
Hugo <hugoarpaulista@gmail.com>
2019-02-06 16:50:53 -0200

    tentando arrumar o integrador

--- Server/CI/application/controllers/Integrador.php
+++ Server/CI/application/controllers/Integrador.php
@@ -1,19 +1,28 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+// require APPPATH . 'libraries/Session/Session.php';
 
-class Usuario extends CI_Controller {
+class Integrador extends CI_Controller {
     function __construct() {
         parent::__construct();
-        $this->load->database('default');
+        $this->load->library('session');
     }
 
-    public function Integrador() {
+    public function Integrar() {
         $dadosRecebidos  = $this->ResolverHttp();
+        var_dump($dadosRecebidos);
+        $retorno = [];
+        $retorno['data'] = date("Y-m-d H:m:s");
         switch ($dadosRecebidos->metodo) {
             case 'usuario/Logar':
-            
-                break;
+            $this->incluirArquivoController('Usuario');
+            $classController = new Usuario();
+            $retorno = $classController->Login($dadosRecebidos);
+            break;
+        
         }
+
+        echo json_encode($retorno);
     }
 
     public function ResolverHttp() {
@@ -37,12 +46,21 @@ class Usuario extends CI_Controller {
             exit(0);
         }
         $postData = file_get_contents("php://input");
+        
         if (isset($postData)) {
-            $pdata = json_decode($postData);
-
-            return $pdata;
+            
+            
+            return json_decode($postData);
+        } else {
+            return '{}';
         }
     }
     
+    private function incluirArquivoController($nomeController) {
+
+        if (!@include($nomeController . ".php")) {
+            throw new Exception("Erro ao incluir Controller: " . $nomeController);
+        }
+    }
 
 }
--- Server/CI/application/controllers/Usuario.php
+++ Server/CI/application/controllers/Usuario.php
@@ -4,18 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Usuario extends CI_Controller {
     function __construct() {
         parent::__construct();
-        $this->load->database('default');
+        // $this->load->library('session');
     }
     
-
-    public function Teste($id) {
-        $this->db->select('*');
-        $this->db->where('id_core_sistema', $id);
-        $retornodb = $this->db->get("core_sistema")->result();
-        var_dump($retornodb);
-    }
-
-    public function Login() {
-        
+    public function Login($dadosRecebidos) {
+        var_dump($dadosRecebidos);
     }
 }
